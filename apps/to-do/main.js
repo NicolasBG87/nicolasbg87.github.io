dc0325b5b6e35bce678c4abcaee5ae59fe5bfcc1
Nikola Bojanovic <nicolas_beograd@ymail.com>
2017-12-21 14:35:54 +0100

    Finished to do app

--- apps/to-do/main.js
+++ apps/to-do/main.js
@@ -1,29 +1,7 @@
-// PENDING ITEMS PLACEHOLDER
-// <div class='container list'><p>" + pendItems + "<span class='pull-right'><i class='fa fa-trash fa-2x remove'></i></span><span class='pull-right'><i class='fa fa-check-circle fa-2x completed'></i></span></p></div>
 
-// COMPLETED ITEMS PLACEHOLDER
-// <div class='container list'><p>" + compItems + "<span class='pull-right'><i class='fa fa-times-circle fa-2x delete'></i></span></p></div>
-
-/*
-# USER INPUTS TEXT
-# USER CLICKS ADD BUTTON
-# STORE DATA TO PENDING ITEMS
-# STORE DATA TO LOCAL STORAGE
-
-# USER CLICKS COMPLETED BUTTON
-# STORE DATA TO COMPLETED ITEMS
-# STORE DATA TO LOCAL STORAGE
-
-# USER CLICKS TRASH BUTTON
-# REMOVE ITEM
-
-# USER CLICKS CROSS BUTTON
-# REMOVE ITEM
-*/
 displayPending();
 
 document.getElementById("add").addEventListener("click", addData);
-document.getElementsByClassName("remove").addEventListener("click", removeData);
 
 function addData() {
   let userInput = item.value;
@@ -47,7 +25,15 @@ function displayPending() {
 
   if(localStorage.getItem("pending")){
     for (let i = 0; i < sItems.length; i++){
-      document.getElementById("pending-items").innerHTML += "<div class='container list'><p>" + sItems[i] + "<span class='pull-right'><i class='fa fa-trash fa-2x remove' id='" + i + "'></i></span><span class='pull-right'><i class='fa fa-check-circle fa-2x' id='completed'></i></span></p></div>";
+      document.getElementById("pending-items").innerHTML += "<div class='container list' id='" + i + "ab'><p>" + sItems[i] + "<span class='pull-right'><i class='fa fa-trash fa-2x' onclick='removeData(this.id)' id='"+ i +"'></i></span></p></div>";
     }
   }
 }
+
+function removeData(e) {
+  let pItems = JSON.parse(localStorage.getItem("pending"));
+  pItems.splice(e, 1);
+  localStorage.setItem("pending", JSON.stringify(pItems));
+  displayPending();
+}
+
