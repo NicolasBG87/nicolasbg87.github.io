a32422509786f797c5c495136041d0be8e97c0cf
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-03-18 22:45:40 +0100

    reword

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 AOS.init({duration: 1200});
+
+const year = document.getElementById('fullYear');
+year.innerHTML = new Date().getFullYear();
+
+const navItems = document.querySelectorAll('.navigation__link');
+const navCheckbox = document.getElementById('navi-toggle');
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navCheckbox.checked = false;
+  });
+});
